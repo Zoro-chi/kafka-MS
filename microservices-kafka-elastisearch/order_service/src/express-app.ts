09336b23c0b1decc02ca43289a68aa0dd1908868
Zoro <jagbetuyi001@gmail.com>
2024-08-03 11:54:04 +0100

    added and implemented kafka as a message broker

--- microservices-kafka-elastisearch/order_service/src/express-app.ts
+++ microservices-kafka-elastisearch/order_service/src/express-app.ts
@@ -1,24 +1,41 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { Consumer, Producer } from "kafkajs";
 
 import orderRoutes from "./routes/order.routes";
 import cartRoutes from "./routes/cart.routes";
 import { httpLogger, HandleErrorWithLogger } from "./utils";
+import { MessageBroker } from "./utils/broker";
 
-const app = express();
+export const ExpressApp = async () => {
+	const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(httpLogger);
+	app.use(cors());
+	app.use(express.json());
+	app.use(httpLogger);
 
-// ROUTES
-app.use(orderRoutes);
-app.use(cartRoutes);
+	// 1st step: Connect producer and consumer
+	const producer = await MessageBroker.connectProducer<Producer>();
+	producer.on("producer.connect", () => console.log("Producer connected"));
 
-app.use("/", (req: Request, res: Response, _: NextFunction) => {
-	return res.status(200).json({ message: "Health check is ok" });
-});
+	const consumer = await MessageBroker.connectConsumer<Consumer>();
+	consumer.on("consumer.connect", () => console.log("Consumer connected"));
 
-app.use(HandleErrorWithLogger);
+	// 2nd step: subscribe to topic or publish message
+	await MessageBroker.subscribe((message) => {
+		console.log("consumer recieved message");
+		console.log("message recieved", message);
+	}, "OrderEvents");
 
-export default app;
+	// ROUTES
+	app.use(orderRoutes);
+	app.use(cartRoutes);
+
+	app.use("/", (req: Request, res: Response, _: NextFunction) => {
+		return res.status(200).json({ message: "Health check is ok" });
+	});
+
+	app.use(HandleErrorWithLogger);
+
+	return app;
+};
--- microservices-kafka-elastisearch/order_service/src/routes/order.routes.ts
+++ microservices-kafka-elastisearch/order_service/src/routes/order.routes.ts
@@ -1,8 +1,31 @@
 import express, { NextFunction, Request, Response } from "express";
+import { MessageBroker } from "../utils/broker";
+import { OrderEvent } from "../types";
 
 const router = express.Router();
 
 router.post("/order", async (req: Request, res: Response, _: NextFunction) => {
+	// Order create logic
+
+	// 3rd step: Publish message
+	await MessageBroker.publish({
+		topic: "OrderEvents",
+		headers: { token: req.headers.authorization },
+		event: OrderEvent.CREATE_ORDER,
+		message: {
+			orderId: 1,
+			items: [
+				{
+					productId: 1,
+					quantity: 1,
+				},
+				{
+					productId: 2,
+					quantity: 2,
+				},
+			],
+		},
+	});
 	return res.status(200).json({ message: "order route is working" });
 });
 
